0ca635e8bb20effb41740ba65c80a0c67c729c3f
Gundarahul1199 <160627205+Gundarahul1199@users.noreply.github.com>
2025-07-01 19:45:31 +0530

    task

--- userfrountend/src/context/StoreContext.jsx
+++ userfrountend/src/context/StoreContext.jsx
@@ -7,7 +7,7 @@ export const StoreContext = createContext();
 const StoreContextProvider = ({children}) => {
     const [cartItems, setCartItems] = useState({});
     const [food_list,setFoodList] = useState([]);
-    const url = 'http://localhost:4000'
+    const url = 'https://foodprepbackend-fnit.onrender.com'
 
     const [token,setToken] = useState("")
     const fetchFoodList = async()=>{
